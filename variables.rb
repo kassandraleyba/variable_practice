c58d69cda3dd492ad9748ecd3c797b183f1de617
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-10-15 12:15:50 -0600

    Add new strings and ruby variables

--- variables.rb
+++ variables.rb
@@ -1,8 +1,5 @@
-name = "Kassandra"
-email = "kassandra.leyba@gmail.com"
-age = 30
-days = 8
-deposit = 1200.00
-payment_due = 100.00
-deposit_paid = false
-payment_due = true
+name = "Paige"
+email = "test@gmail.com"
+puts 
+puts
+puts
