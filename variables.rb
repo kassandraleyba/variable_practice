750ad9728285979cfad7d616aa199c3c8735a9aa
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-10-17 17:31:18 -0600

    Add floats #2, +1 string, +2 integer

--- variables.rb
+++ variables.rb
@@ -1,6 +1,13 @@
 first_name = "Kassandra"
 last_name = "Leyba"
 email = "kassandra.leyba@gmail.com"
+profile_bio = "BE 2211"
 
 age = 30
 day = 8
+month = 10
+number_of_books = 50
+
+deposit = 1200.00
+ticket_price = 295.00
+sale_price = 12.99
