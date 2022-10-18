e0d3731d11d37cf1b80a17d4af4435c4d1f9f2b9
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-10-18 06:56:32 -0600

    Add corrections & tips

--- variables.rb
+++ variables.rb
@@ -1,20 +1,35 @@
 first_name = "Kassandra"
-first_name #Kassandra
+puts first_name
+#prints "Kassandra" to the console
+
 last_name = "Leyba"
-last_name #Leyba
+puts last_name
+#p "Leyba"
+
 email = "kassandra.leyba@gmail.com"
-email #kassandra.leyba@gmail.com
+puts email
+#p "kassandra.leyba@gmail.com"
+
 profile_bio = "BE 2211"
-profile_bio #BE 2211
+puts profile_bio
+#p "BE 2211"
 
 first_name = "John"
-first_name #John
+puts first_name
+#p "John" to the console now that I've reassigned first_name
+
 last_name = "Smith"
-last_name #Smith
+puts last_name
+#p "Smith"
+
 email = "johnsmith@gmail.com"
-email #johnsmith@gmail.com
+puts email
+#p "johnsmith@gmail.com"
+
 profile_bio = "FE 2211"
-profile_bio #FE 2211
+puts profile_bio
+#p "FE 2211"
+
 
 age = 30
 day = 8
