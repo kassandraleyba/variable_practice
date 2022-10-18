870b5fe86b8495bc1dcc7ab363583d47cc0180b5
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-10-17 18:08:26 -0600

    Add reassigned strings

--- variables.rb
+++ variables.rb
@@ -1,7 +1,20 @@
 first_name = "Kassandra"
+first_name #Kassandra
 last_name = "Leyba"
+last_name #Leyba
 email = "kassandra.leyba@gmail.com"
+email #kassandra.leyba@gmail.com
 profile_bio = "BE 2211"
+profile_bio #BE 2211
+
+first_name = "John"
+first_name #John
+last_name = "Smith"
+last_name #Smith
+email = "johnsmith@gmail.com"
+email #johnsmith@gmail.com
+profile_bio = "FE 2211"
+profile_bio #FE 2211
 
 age = 30
 day = 8
